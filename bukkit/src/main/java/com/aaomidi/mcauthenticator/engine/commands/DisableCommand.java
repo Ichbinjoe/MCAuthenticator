203e67e1071ca4b993659009de856ac8696c2c68
Joseph Hirschfeld <joe@ibj.io>
2016-09-02 23:15:29 -0400

    Add bungee plugin

---
+++ bukkit/src/main/java/com/aaomidi/mcauthenticator/engine/commands/DisableCommand.java
@@ -0,0 +1,125 @@
+package com.aaomidi.mcauthenticator.engine.commands;
+
+import com.aaomidi.mcauthenticator.MCAuthenticator;
+import com.aaomidi.mcauthenticator.model.User;
+import com.aaomidi.mcauthenticator.model.UserData;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+/**
+ * @author Joseph Hirschfeld <joe@ibj.io>
+ * @date 2/1/16
+ */
+public final class DisableCommand extends AuthCommand {
+    public DisableCommand(MCAuthenticator instance) {
+        super(instance, "disable", "mcauthenticator.disable", "Disables 2FA on an account");
+    }
+
+    @Override
+    public boolean execute(Command command, final CommandSender commandSender, String[] args) {
+        if (args.length == 0) {
+            //Self
+            if (!(commandSender instanceof Player)) {
+                getInstance().getC().sendDirect(commandSender, "&cYou must specify a player to enable 2FA on.\n" +
+                        "&c    /auth disable <player>");
+                return true;
+            }
+
+            Player sndr = ((Player) commandSender);
+            User u = getInstance().getCache().get(sndr.getUniqueId());
+            if (!(u.is2fa() && !u.mustSetUp2FA())) { //Fully set up, we can cancel 'init' staged 2FA
+                getInstance().getC().send(commandSender, getInstance().getC().message("alreadyDisabled"));
+                return true;
+            }
+
+            if (u.isLocked(sndr)) {
+                getInstance().getC().send(commandSender, getInstance().getC().message("disableForced"));
+                return true;
+            }
+
+            u.invalidateKey(sndr);
+            u.reverseInventory(sndr);
+            getInstance().save();
+
+            getInstance().getC().send(commandSender, getInstance().getC().message("selfDisabled"));
+        } else if (args.length == 1) {
+            if (!commandSender.hasPermission("mcauthenticator.disable.other") && !isConsole(commandSender)) {
+                getInstance().getC().sendDirect(commandSender, "&cYou are not permitted to disable other people's 2FA!");
+                return true;
+            }
+            final String playerQuery = args[0];
+            getInstance().async(new Runnable() {
+                @Override
+                public void run() {
+                    final OfflinePlayer player = Bukkit.getOfflinePlayer(playerQuery);
+                    if (player == null) {
+                        getInstance().getC().sendDirect(commandSender, "&cThe player &4'" + playerQuery + "'&c does not exist!");
+                        return;
+                    }
+
+                    if (player.isOnline()) {
+                        final User u = getInstance().getCache().get(player.getUniqueId());
+                        if (!(u.is2fa() && !u.mustSetUp2FA())) {
+                            getInstance().getC().sendDirect(commandSender, "&cThe player &4'" + playerQuery + "'&c already has 2FA disabled.");
+                            return;
+                        }
+
+                        final Player onlinePlayer = (Player) player;
+                        if (u.isLocked(onlinePlayer)) {
+                            getInstance().getC().sendDirect(commandSender, "&cYou cannot disable 2FA on the player's account since they have the locked permission.");
+                            return;
+                        }
+
+                        getInstance().sync(new Runnable() {
+                            @Override
+                            public void run() {
+                                getInstance().getC().send(onlinePlayer, getInstance().getC().message("otherDisable").replaceAll("%player%", commandSender.getName()));
+                                getInstance().getC().sendDirect(commandSender, "&7You have disabled 2FA on " + player.getName() + "'s account.");
+                                u.invalidateKey(onlinePlayer);
+                                u.reverseInventory(onlinePlayer);
+                                getInstance().save();
+                            }
+                        });
+                    } else {
+                        //Player isn't online, so lets just kinda 'help them along'
+                        //Lets first check if they already have a record.
+
+                        UserData d;
+
+                        try {
+                            d = getInstance().getDataSource().getUser(player.getUniqueId());
+                        } catch (IOException | SQLException e) {
+                            commandSender.sendMessage(ChatColor.RED + "There was an issue retrieving the userdata. Check console.");
+                            getInstance().handleException(e);
+                            return;
+                        }
+
+                        if (d != null) {
+                            if (d.isLocked(null) && !(commandSender instanceof ConsoleCommandSender)) {
+                                getInstance().getC().sendDirect(commandSender, "&c This user has the locked permission! You cannot disable 2FA for this person unless you are in console!");
+                                return;
+                            }
+                            //Take out the entry
+                            getInstance().getDataSource().destroyUser(player.getUniqueId());
+                            getInstance().getC().sendDirect(commandSender, "&7Disabled 2FA for " + player.getName() + ".");
+                            getInstance().save();
+                        } else {
+                            getInstance().getC().sendDirect(commandSender, "&4'" + player.getName() + "'&c already has 2FA disabled!");
+                        }
+                    }
+                }
+            });
+        } else {
+            getInstance().getC().sendDirect(commandSender, "&c Invalid usage: /auth disable" + (commandSender.hasPermission("mcauthenticator.disable.other") ? " [player]" : ""));
+        }
+        return true;
+    }
+}
---
+++ bukkit/src/main/java/com/aaomidi/mcauthenticator/model/User.java
@@ -0,0 +1,164 @@
+package com.aaomidi.mcauthenticator.model;
+
+import com.aaomidi.mcauthenticator.MCAuthenticator;
+import com.aaomidi.mcauthenticator.auth.Authenticator;
+import lombok.Getter;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.UUID;
+
+/**
+ * @author Joseph Hirschfeld <joe@ibj.io>
+ * @date 1/29/16
+ */
+public final class User {
+
+    public User(UUID playerId, UserData userData,
+                MCAuthenticator mcAuthenticator) {
+        this.playerId = playerId;
+        this.userData = userData;
+        this.mcAuthenticator = mcAuthenticator;
+    }
+
+    private final UUID playerId;
+
+    @Getter
+    private UserData userData;
+    private final MCAuthenticator mcAuthenticator;
+
+    @Getter
+    private boolean isInit = false;
+
+    //Whether or not the user is viewed as 'authenticated'
+    private boolean authenticated = false;
+    private boolean lastAuthStatusUpstream = false;
+    private boolean hasSentAuthStatusRemote = false;
+
+    private ItemStack[] inventory = null;
+
+    public boolean authenticated() {
+        return !isInit && (userData == null || authenticated);
+    }
+
+    public boolean mustSetUp2FA() {
+        return userData != null && userData.getSecret() == null;
+    }
+
+    public boolean authenticate(Player p) {
+        if (authenticated()) return true;
+        if (!mcAuthenticator.getC().isEnforceSameIPAuth() &&
+                userData.getLastAddress() != null &&
+                userData.getLastAddress().equals(p.getAddress().getAddress())) {
+
+            isInit = false;
+            authenticated = true;
+        }
+        sendAuthenticatedUpstream(p);
+        return authenticated;
+    }
+
+    public boolean is2fa() {
+        return userData != null;
+    }
+
+    public boolean authenticate(String message, Player player)
+            throws Exception {
+        boolean authenticate = false;
+        for (Authenticator a : mcAuthenticator.getAuthenticators()) {
+            if (a.isFormat(message)) {
+                authenticate = a.authenticate(this, player, message);
+                if (authenticate) break;
+            }
+        }
+
+        if (!authenticate) {
+            sendAuthenticatedUpstream(player);
+            return false;
+        }
+
+        isInit = false;
+        authenticated = true;
+
+        reverseInventory(player);
+        userData.setLastAddress(player.getAddress().getAddress());
+        userData.setLocked(player.hasPermission("mcauthenticator.lock"));
+        mcAuthenticator.save();
+        sendAuthenticatedUpstream(player);
+        return true;
+    }
+
+    public void remoteAuthenticated() {
+        System.out.println("REMOTE AUTH");
+        isInit = false;
+        authenticated = true;
+    }
+
+    public void invalidateKey(Player p) {
+        mcAuthenticator.getDataSource().destroyUser(userData.getId());
+        userData = null;
+        authenticated = false;
+        sendAuthenticatedUpstream(p);
+    }
+
+    public boolean init2fa(Player p) {
+        if (userData != null && userData.getSecret() != null) { //Already has a secret
+            return false;
+        }
+
+        for (Authenticator a : mcAuthenticator.getAuthenticators()) {
+            a.initUser(this, p);
+        }
+
+        isInit = true;
+        sendAuthenticatedUpstream(p);
+
+        return true;
+    }
+
+    public void setUserInfo(String secret, int authtype, Player p) {
+        if (userData == null)
+            userData = mcAuthenticator.getDataSource().createUser(playerId);
+        userData.setSecret(secret, authtype);
+        sendAuthenticatedUpstream(p);
+    }
+
+    public boolean isLocked(Player p) {
+        return userData != null && userData.isLocked(p);
+    }
+
+    public boolean isInventoryStored() {
+        return inventory != null;
+    }
+
+    public void storeInventory(Player p) {
+        if (inventory != null)
+            throw new IllegalStateException("Cannot double store inventory!");
+        inventory = p.getInventory().getContents();
+        p.getInventory().setContents(new ItemStack[36]);
+    }
+
+    public void reverseInventory(Player p) {
+        if (inventory != null) {
+            p.getInventory().setContents(inventory);
+            inventory = null;
+        }
+    }
+
+    public void sendAuthenticatedUpstream(final Player p) {
+        final String authChannel = mcAuthenticator.getC().getBungeePluginChannel();
+        if (authChannel != null && p != null) {
+            final boolean authStatus = authenticated();
+            if (!hasSentAuthStatusRemote || lastAuthStatusUpstream != authStatus) {
+                hasSentAuthStatusRemote = true;
+                mcAuthenticator.sync(new Runnable() {
+                    @Override
+                    public void run() {
+                        p.sendPluginMessage(mcAuthenticator, authChannel, (authStatus ? new byte[]{0x00} : new byte[]{0x01}));
+                    }
+                }, 3); /// 3 ticks seems to be the magic time. For some reason, we can't send upstream immediately on log in....?
+                lastAuthStatusUpstream = authStatus;
+            }
+        }
+    }
+}
